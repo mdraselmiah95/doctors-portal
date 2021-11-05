0cc7f0e08b68422274a1cba2ddda52cabb4c34e8
rasel <russelroy15@gmail.com>
2021-11-06 00:50:31 +0600

    WIP on Doctors Portal

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import React from "react";
@@ -32,6 +32,16 @@ const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
+        <Typography
+          sx={{ fontWeight: 500, m: 2, color: "info.main" }}
+          variant="h5"
+          component="div"
+        >
+          OUR SERVICES
+        </Typography>
+        <Typography sx={{ fontWeight: 600, m: 3 }} variant="h4" component="div">
+          Services We Provide
+        </Typography>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
