be6850462ee3869ebbf03d4e8c1cd4b16cfacc07
rasel <russelroy15@gmail.com>
2021-11-06 00:08:08 +0600

    WIP on Doctors portal project

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,40 +1,32 @@
 import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import { experimentalStyled as styled } from "@mui/material/styles";
 import React from "react";
 import cavity from "../../../images/cavity.png";
 import fluoride from "../../../images/fluoride.png";
 import whitening from "../../../images/whitening.png";
+import Service from "../Service/Service";
 
 const services = [
   {
     name: "Fluoride Treatment",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+      "If your cavity just started, a fluoride treatment may help restore your tooth's enamel and can sometimes reverse a cavity in the very early stages",
     img: fluoride,
   },
   {
     name: "Cavity Filling",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+      "Regular checkups can identify cavities and other dental conditions before they cause troubling symptoms and lead to more-serious problems",
     img: cavity,
   },
   {
     name: "Teeth Whitening",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+      "sparkle back into your smile! Our whitenings are both easy and non-invasive, and you can remove years of stain and color from your teeth in a single, one-hour treatment.",
     img: whitening,
   },
 ];
-
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 const Services = () => {
   return (
@@ -47,7 +39,7 @@ const Services = () => {
         >
           {Array.from(Array(6)).map((_, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
+              <Service />
             </Grid>
           ))}
         </Grid>
