081aa0cf90110077c6b17b87c9a9d288024af33e
rasel <russelroy15@gmail.com>
2021-11-06 00:32:55 +0600

    WIP on Doctors portal project

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,10 +1,33 @@
+import { Grid } from "@mui/material";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import React from "react";
-
 const Service = () => {
   return (
-    <div>
-      <h2>This is Service</h2>
-    </div>
+    <Grid item xs={2} sm={4} md={4}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="div"></Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -37,10 +37,8 @@ const Services = () => {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Service />
-            </Grid>
+          {services.map((service) => (
+            <Service key={service.name} service={service} />
           ))}
         </Grid>
       </Container>
