4fc115b3e313e3ae341125547f5587aae18dbd15
rasel <russelroy15@gmail.com>
2021-11-06 01:36:37 +0600

    WIP on Doctors Portal

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,12 +1,27 @@
 import { Button, Container, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import bg from "../../../images/bg.png";
 import chair from "../../../images/chair.png";
+const bannerBg = {
+  background: `url(${bg})`,
+};
+
+const verticalCenter = {
+  display: "flex",
+  alignItems: "center",
+  height: 400,
+};
 const Banner = () => {
   return (
-    <Container sx={{ flexGrow: 1 }}>
+    <Container sx={{ flexGrow: 1 }} style={bannerBg}>
       <Grid container spacing={2}>
-        <Grid item style={{ textAlign: "left" }} xs={12} md={6}>
+        <Grid
+          item
+          style={{ ...verticalCenter, textAlign: "left" }}
+          xs={12}
+          md={6}
+        >
           <Box>
             <Typography variant="h3">
               Your New Smile <br />
@@ -24,7 +39,7 @@ const Banner = () => {
             </Button>
           </Box>
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} style={verticalCenter}>
           <img style={{ width: "350px" }} src={chair} alt="" />
         </Grid>
       </Grid>
