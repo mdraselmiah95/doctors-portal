a4f8947b6a34839e3e881fde8364b53e415572d3
rasel <russelroy15@gmail.com>
2021-11-05 23:32:55 +0600

    WIP on Doctors portal project

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navigation from "../../Shared/Navigation/Navigation";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Navigation />
     </div>
   );
 };
