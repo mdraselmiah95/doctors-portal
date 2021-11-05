e6b366d3ca3ecae7256909437318b633efcddd78
rasel <russelroy15@gmail.com>
2021-11-06 01:02:24 +0600

    WIP on Doctors Portal

--- src/pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -1,10 +1,23 @@
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import React from "react";
+import doctor from "../../../images/doctor.png";
 
 const AppointmentBanner = () => {
   return (
-    <div>
-      <h2>AppointmentBanner</h2>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <img src={doctor} alt="doctor" style={{ width: 400 }} />
+        </Grid>
+        <Grid item xs={4} md={6}>
+          <Typography variant="h5" sx={{ mb: 5 }} style={{ color: "#5CE7ED" }}>
+            Appointment
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
