8f8bbfd1b2f65cc240a61811028653f9d46e5fc1
rasel <russelroy15@gmail.com>
2021-11-06 00:38:05 +0600

    WIP on Doctors portal project

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,31 +1,26 @@
-import { Grid } from "@mui/material";
-import Button from "@mui/material/Button";
+import { CardMedia, Grid } from "@mui/material";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import React from "react";
-const Service = () => {
+const Service = (props) => {
+  const { description, img, name } = props.service;
   return (
     <Grid item xs={2} sm={4} md={4}>
       <Card sx={{ minWidth: 275 }}>
+        <CardMedia
+          component="img"
+          style={{ width: "auto", height: "76px", margin: "0 auto" }}
+          image={img}
+          alt="Paella dish"
+        />
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
           <Typography variant="h5" component="div"></Typography>
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
+            {name}
           </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
+          <Typography variant="body2">{description}</Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
       </Card>
     </Grid>
   );
