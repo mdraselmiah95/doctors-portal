d86e3639582eaaf354e6890f38b7a8939f50bb41
rasel <russelroy15@gmail.com>
2021-11-05 23:49:33 +0600

    WIP on Doctors portal project

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,10 +1,52 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { experimentalStyled as styled } from "@mui/material/styles";
 import React from "react";
+
+// const services = [
+//   {
+//     name: "Fluoride Treatment",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+//     img: fluoride,
+//   },
+//   {
+//     name: "Cavity Filling",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+//     img: cavity,
+//   },
+//   {
+//     name: "Teeth Whitening",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+//     img: whitening,
+//   },
+// ];
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const Services = () => {
   return (
-    <div>
-      <h2>This is Services</h2>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            <Item>xs=2</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
