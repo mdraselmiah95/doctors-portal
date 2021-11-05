e841e5477f0026ca0ad1a71e3a572c36b4f7707e
rasel <russelroy15@gmail.com>
2021-11-06 00:01:14 +0600

    WIP on Doctors portal project

---
+++ src/pages/Home/Service/Service.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div>
+      <h2>This is Service</h2>
+    </div>
+  );
+};
+
+export default Service;
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,29 +1,33 @@
+import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { experimentalStyled as styled } from "@mui/material/styles";
 import React from "react";
+import cavity from "../../../images/cavity.png";
+import fluoride from "../../../images/fluoride.png";
+import whitening from "../../../images/whitening.png";
 
-// const services = [
-//   {
-//     name: "Fluoride Treatment",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
-//     img: fluoride,
-//   },
-//   {
-//     name: "Cavity Filling",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
-//     img: cavity,
-//   },
-//   {
-//     name: "Teeth Whitening",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
-//     img: whitening,
-//   },
-// ];
+const services = [
+  {
+    name: "Fluoride Treatment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+    img: fluoride,
+  },
+  {
+    name: "Cavity Filling",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+    img: cavity,
+  },
+  {
+    name: "Teeth Whitening",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+    img: whitening,
+  },
+];
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -35,17 +39,19 @@ const Item = styled(Paper)(({ theme }) => ({
 const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
-            <Item>xs=2</Item>
-          </Grid>
-        ))}
-      </Grid>
+      <Container>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {Array.from(Array(6)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Item>xs=2</Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </Box>
   );
 };
