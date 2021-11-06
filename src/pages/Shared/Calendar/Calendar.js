465726651dc9d222bd934763b883c777c38acc4e
rasel <russelroy15@gmail.com>
2021-11-06 09:32:03 +0600

    WIP on doctor portal

--- src/pages/Shared/Calendar/Calendar.js
+++ src/pages/Shared/Calendar/Calendar.js
@@ -1,10 +1,25 @@
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import StaticDatePicker from "@mui/lab/StaticDatePicker";
+import TextField from "@mui/material/TextField";
+import isWeekend from "date-fns/isWeekend";
 import React from "react";
 
 const Calendar = () => {
+  const [value, setValue] = React.useState(new Date());
   return (
-    <div>
-      <h2>Calendar</h2>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDatePicker
+        orientation="landscape"
+        openTo="day"
+        value={value}
+        shouldDisableDate={isWeekend}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
   );
 };
 
