94baa1d89cf8e336454e476c0c8333e31682083a
rasel <russelroy15@gmail.com>
2021-11-06 08:56:01 +0600

    WIP on doctor portal

--- src/pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,16 @@
+import { Container, Grid } from "@mui/material";
 import React from "react";
-
+import chair from "../../../images/chair.png";
 const AppointmentHeader = () => {
   return (
-    <div>
-      <h2>AppointmentHeader</h2>
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}></Grid>
+        <Grid item xs={12} md={6}>
+          <img src={chair} alt="chair" style={{ width: "100%" }} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
