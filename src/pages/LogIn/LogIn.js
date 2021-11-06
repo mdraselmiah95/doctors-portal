12e1733810a43a987fa9101b4c8e9657033382b5
rasel <russelroy15@gmail.com>
2021-11-06 22:25:57 +0600

    WIP on Doctors Portal

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,10 +1,56 @@
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const LogIn = () => {
   return (
-    <div>
-      <h2>LogIn</h2>
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item sx={{ mt: 8 }} xs={12} md={6}>
+          <Typography variant="body1" gutterBottom>
+            Login
+          </Typography>
+          <form onSubmit={handleLoginSubmit}>
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-basic"
+              label="Your Email"
+              name="email"
+              //   onChange={handleOnChange}
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-basic"
+              label="Your Password"
+              type="password"
+              name="password"
+              //   onChange={handleOnChange}
+              variant="standard"
+            />
+
+            <Button
+              sx={{ width: "75%", m: 1 }}
+              type="submit"
+              variant="contained"
+            >
+              Login
+            </Button>
+            <NavLink style={{ textDecoration: "none" }} to="/register">
+              <Button variant="text">New User? Please Register</Button>
+            </NavLink>
+            {/* {isLoading && <CircularProgress />}
+            {user?.email && (
+              <Alert severity="success">Login successfully!</Alert>
+            )}
+            {authError && <Alert severity="error">{authError}</Alert>} */}
+          </form>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img style={{ width: "100%" }} src={login} alt="" />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
