2f20ec8ef29b4777a8f84546ae93eae74282e0a4
rasel <russelroy15@gmail.com>
2021-11-06 23:32:54 +0600

    WIP on Doctors Portal

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,7 +1,46 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 import initializeFirebase from "../pages/LogIn/Firebase/firebase.init";
 
 initializeFirebase();
 
-const useFirebase = () => {};
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
+  const registerUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return {
+    user,
+    registerUser,
+    logout,
+  };
+};
 
 export default useFirebase;
