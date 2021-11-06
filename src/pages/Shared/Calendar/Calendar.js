db762584e24b2742e2b73dada647fbe45eaa94fa
rasel <russelroy15@gmail.com>
2021-11-06 09:36:22 +0600

    WIP on doctor portal

--- src/pages/Shared/Calendar/Calendar.js
+++ src/pages/Shared/Calendar/Calendar.js
@@ -2,7 +2,6 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import StaticDatePicker from "@mui/lab/StaticDatePicker";
 import TextField from "@mui/material/TextField";
-import isWeekend from "date-fns/isWeekend";
 import React from "react";
 
 const Calendar = () => {
@@ -10,10 +9,8 @@ const Calendar = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <StaticDatePicker
-        orientation="landscape"
-        openTo="day"
+        displayStaticWrapperAs="desktop"
         value={value}
-        shouldDisableDate={isWeekend}
         onChange={(newValue) => {
           setValue(newValue);
         }}
