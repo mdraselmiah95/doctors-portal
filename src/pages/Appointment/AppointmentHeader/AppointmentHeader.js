43fcf4c38ddb8cafce9c5f832ec9b667064ee195
rasel <russelroy15@gmail.com>
2021-11-06 09:05:00 +0600

    WIP on doctor portal

--- src/pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,11 +1,14 @@
 import { Container, Grid } from "@mui/material";
 import React from "react";
 import chair from "../../../images/chair.png";
+import Calendar from "../../Shared/Calendar/Calendar";
 const AppointmentHeader = () => {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}></Grid>
+        <Grid item xs={12} md={6}>
+          <Calendar />
+        </Grid>
         <Grid item xs={12} md={6}>
           <img src={chair} alt="chair" style={{ width: "100%" }} />
         </Grid>
