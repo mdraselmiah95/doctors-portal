416eb9884a2732f8a312f6ad479cba3b6789a3ea
rasel <russelroy15@gmail.com>
2021-11-07 14:02:34 +0600

    WIP on Doctors Portal

--- src/pages/Dashboard/Appointments/Appointments.js
+++ src/pages/Dashboard/Appointments/Appointments.js
@@ -1,3 +1,10 @@
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import React, { useEffect, useState } from "react";
 import useAuth from "../../../hooks/useAuth";
 
@@ -14,6 +21,33 @@ const Appointments = () => {
   return (
     <div>
       <h2>Appointments {appointments.length}</h2>
+      <TableContainer component={Paper}>
+        <Table sx={{}} aria-label="Appointments table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Time</TableCell>
+              <TableCell align="right">Service</TableCell>
+              <TableCell align="right">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {appointments.map((row) => (
+              <TableRow
+                key={row._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.patientName}
+                </TableCell>
+                <TableCell align="right">{row.time}</TableCell>
+                <TableCell align="right">{row.serviceName}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
