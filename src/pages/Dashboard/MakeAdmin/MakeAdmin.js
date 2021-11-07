87c6afdeeb0166178d21b9991d56fb9583a5fb73
rasel <russelroy15@gmail.com>
2021-11-08 02:47:55 +0600

    WIP on Doctors Portal

--- src/pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,28 @@
-import React from "react";
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const MakeAdmin = () => {
+  const [email, setEmail] = useState("");
+
+  const handleAdminSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h2>MakeAdmin</h2>
+      <h2>Make an Admin</h2>
+      <form onSubmit={handleAdminSubmit}>
+        <TextField
+          sx={{ width: "50%" }}
+          label="Email"
+          type="email"
+          //   onBlur={handleOnBlur}
+          variant="standard"
+        />
+        <Button type="submit" variant="contained">
+          Make Admin
+        </Button>
+      </form>
+      {/* {success && <Alert severity="success">Made Admin successfully!</Alert>} */}
     </div>
   );
 };
