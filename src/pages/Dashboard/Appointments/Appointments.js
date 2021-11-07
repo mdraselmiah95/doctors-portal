4c9d6f26bd433751683d64c044d0988a64dffb8d
rasel <russelroy15@gmail.com>
2021-11-07 13:54:24 +0600

    WIP on Doctors Portal

--- src/pages/Dashboard/Appointments/Appointments.js
+++ src/pages/Dashboard/Appointments/Appointments.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const Appointments = () => {
+  const { user } = useAuth();
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/appointments?email=${user.email}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAppointments(data));
+  }, []);
   return (
     <div>
-      <h2>Appointments</h2>
+      <h2>Appointments {appointments.length}</h2>
     </div>
   );
 };
