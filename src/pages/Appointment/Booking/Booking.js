55e72ade8190b2c48dad9983824186686d90dd4f
rasel <russelroy15@gmail.com>
2021-11-07 12:54:28 +0600

    WIP on Doctors Portal

--- src/pages/Appointment/Booking/Booking.js
+++ src/pages/Appointment/Booking/Booking.js
@@ -5,7 +5,7 @@ import Typography from "@mui/material/Typography";
 import React from "react";
 import BookingModal from "../BookingModal/BookingModal";
 
-const Booking = ({ booking, date }) => {
+const Booking = ({ booking, date, setBookingSuccess }) => {
   const { name, time, space } = booking;
   const [openBooking, setBookingOpen] = React.useState(false);
   const handleBookingOpen = () => setBookingOpen(true);
@@ -43,6 +43,7 @@ const Booking = ({ booking, date }) => {
         booking={booking}
         openBooking={openBooking}
         handleBookingClose={handleBookingClose}
+        setBookingSuccess={setBookingSuccess}
       ></BookingModal>
     </>
   );
