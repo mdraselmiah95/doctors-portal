00dc829911a97b766c64af4ed3d3e29937e4bc14
rasel <russelroy15@gmail.com>
2021-11-08 11:50:27 +0600

    WIP on Doctors Portal

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -17,6 +17,7 @@ const useFirebase = () => {
   const [user, setUser] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [authError, setAuthError] = useState("");
+  const [admin, setAdmin] = useState(false);
 
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
@@ -92,6 +93,12 @@ const useFirebase = () => {
     });
     return () => unsubscribed;
   }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setAdmin(data.admin));
+  }, [user.email]);
 
   const logout = () => {
     setIsLoading(true);
