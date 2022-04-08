470e938a6d5cf6ce607ee4e4f97351bec65b7faf
mdraselmiah95 <russelroy15@gmail.com>
2022-04-09 04:26:27 +0600

    WIP on doctor portal project

--- src/pages/Dashboard/Payment/CheckoutForm.js
+++ src/pages/Dashboard/Payment/CheckoutForm.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { CardElement } from "@stripe/react-stripe-js";
 
 const CheckoutForm = () => {
   return (
     <div>
-      <h2>Hello</h2>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
     </div>
   );
 };
