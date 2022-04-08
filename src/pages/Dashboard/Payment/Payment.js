b0dabae722c67c7193c27b3da8e75aecaae41f0c
mdraselmiah95 <russelroy15@gmail.com>
2022-04-09 04:24:18 +0600

    WIP on doctor portal project

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,12 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51Jw6SKHMt687dAra9sxMSE3nLy5pM9HeNqThwsGNox6xrDosPrH58ciSutmFgE9p4qhGYEBunlsp1WXeNIHBZSdG00BnwCZuqs"
+);
 
 const Payment = () => {
   const { appointmentId } = useParams();
@@ -16,6 +23,9 @@ const Payment = () => {
         Please Pay for: {appointment.patientName} for {appointment.serviceName}
       </h2>
       <h4>Pay: ${appointment.price}</h4>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 };
