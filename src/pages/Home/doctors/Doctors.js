ab0340fb69476bc497e7175d8bbe05f9375407dd
mdraselmiah95 <russelroy15@gmail.com>
2022-04-10 05:39:07 +0600

    WIP on Doctors portal porject

--- src/pages/Home/doctors/Doctors.js
+++ src/pages/Home/doctors/Doctors.js
@@ -5,7 +5,7 @@ const Doctors = () => {
   const [doctors, setDoctors] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/doctors")
+    fetch("https://glacial-wave-71858.herokuapp.com/doctors")
       .then((res) => res.json())
       .then((data) => setDoctors(data));
   }, []);
