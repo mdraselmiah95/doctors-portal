e7f37f7040fbf5ba2bce4febe08965709b3eff73
mdraselmiah95 <russelroy15@gmail.com>
2022-04-10 04:34:36 +0600

    WIP on Doctors portal porject

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AuthProvider from "./contexts/AuthProvider/AuthProvider";
 import Appointment from "./pages/Appointment/Appointment/Appointment";
+import AddDoctor from "./pages/Dashboard/AddDoctor/AddDoctor";
 import Dashboard from "./pages/Dashboard/Dashboard/Dashboard";
+import DashboardHome from "./pages/Dashboard/DashboardHome/DashboardHome";
+import MakeAdmin from "./pages/Dashboard/MakeAdmin/MakeAdmin";
+import Payment from "./pages/Dashboard/Payment/Payment";
 import Home from "./pages/Home/Home/Home";
+import AdminRoute from "./pages/LogIn/AdminRoute/AdminRoute";
 import LogIn from "./pages/LogIn/LogIn/LogIn";
 import PrivateRoute from "./pages/LogIn/PrivateRoute/PrivateRoute";
 import Register from "./pages/LogIn/Register/Register";
@@ -31,7 +36,33 @@ function App() {
                   <Dashboard />
                 </PrivateRoute>
               }
-            />
+            >
+              <Route
+                exact
+                path="/dashboard"
+                element={<DashboardHome></DashboardHome>}
+              ></Route>
+              <Route
+                path={`/dashboard/payment/:appointmentId`}
+                element={<Payment></Payment>}
+              ></Route>
+              <Route
+                path={`/dashboard/makeAdmin`}
+                element={
+                  <AdminRoute>
+                    <MakeAdmin></MakeAdmin>
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path={`/dashboard/addDoctor`}
+                element={
+                  <AdminRoute>
+                    <AddDoctor></AddDoctor>
+                  </AdminRoute>
+                }
+              ></Route>
+            </Route>
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<LogIn />} />
             <Route path="/register" element={<Register />} />
