89dc9d69d7ae59a993d1a48b2311584efc4266d9
mdraselmiah95 <russelroy15@gmail.com>
2022-04-10 03:26:10 +0600

    WIP on Doctors portal porject

--- src/pages/LogIn/PrivateRoute/PrivateRoute.js
+++ src/pages/LogIn/PrivateRoute/PrivateRoute.js
@@ -1,30 +1,18 @@
 import { CircularProgress } from "@mui/material";
 import React from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 
 const PrivateRoute = ({ children, ...rest }) => {
   const { user, isLoading } = useAuth();
+  let location = useLocation();
   if (isLoading) {
     return <CircularProgress />;
   }
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        user.email ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    />
-  );
+  if (user.email) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} />;
 };
 
 export default PrivateRoute;
