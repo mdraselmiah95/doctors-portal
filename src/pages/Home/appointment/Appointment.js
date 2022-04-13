e09c1b337d475a3d0988b0eef432dde61e024580
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 05:55:19 +0600

    WIP on Doctors portal project

--- src/pages/Home/appointment/Appointment.js
+++ src/pages/Home/appointment/Appointment.js
@@ -1,6 +1,21 @@
+import { makeStyles } from "@mui/material";
 import React from "react";
+import bg from "../../../images/appointment-bg.png";
 
 const Appointment = () => {
+  const useStyle = makeStyles({
+    root: {
+      background: `url(${bg}), linear-gradient(#3A4256,#3A4256)`,
+      backgroundBlendMode: "overlay",
+      backgroundRepeat: "no-repeat",
+      height: 380,
+      display: "flex",
+      alignItems: "center",
+      marginBottom: 200,
+      marginTop: 200,
+    },
+  });
+  const { root } = useStyle();
   return (
     <div>
       <h2>gf</h2>
