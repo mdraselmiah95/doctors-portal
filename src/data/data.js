48be162724989c14c3c5044b12cff3a8445b9a34
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 05:33:33 +0600

    New Data added

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import WifiCalling3Icon from "@mui/icons-material/WifiCalling3";
+
+export const infoData = [
+  {
+    title: "Opening Hours",
+    description: "We are open 7 days",
+    Icon: AccessTimeIcon,
+    background: "#1CC7C1",
+  },
+  {
+    title: "Visit Our Location",
+    description: "Brooklyn, NY 10003 USA",
+    Icon: LocationOnIcon,
+    background: "#3A4256",
+  },
+  {
+    title: "Call us now",
+    description: "+15697854124",
+    Icon: WifiCalling3Icon,
+    background: "#1CC7C1",
+  },
+];
