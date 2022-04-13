f1dd8cf99a64ae8f12b9e3a6f0998e9c5383d4b3
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 05:35:15 +0600

    WIP on Doctors portal card info

--- src/pages/Home/InfoCard/InfoCard.js
+++ src/pages/Home/InfoCard/InfoCard.js
@@ -1,10 +1,33 @@
+import { Container, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
+import { infoData } from "../../../data/data";
 
 const InfoCard = () => {
   return (
-    <div>
-      <h2></h2>
-    </div>
+    <Container>
+      <Grid>
+        {infoData.map(({ Icon, title, description, background }) => (
+          <Grid key={title} item xs={12} sm={12} md={4} lg={4}>
+            <Paper
+              variant="outlined"
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                background: background,
+                color: "#fff",
+                p: 3,
+              }}
+            >
+              <Icon sx={{ fontSize: 60 }} />
+              <div>
+                <Typography variant="body1">{title}</Typography>
+                <Typography variant="subtitle1">{description}</Typography>
+              </div>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
