d8792614b93fb7d71ad93d6979ba2d6517ea2cb6
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 06:26:05 +0600

    WIP on Doctors portal project on Our Doctors and Our Single Doctor part

---
+++ src/pages/Home/ourDoctors/OurDoctorSIngle .js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OurDoctorSIngle = () => {
+  return (
+    <div>
+      <h1>f</h1>
+    </div>
+  );
+};
+
+export default OurDoctorSIngle;
--- src/pages/Home/ourDoctors/OurDoctors.js
+++ src/pages/Home/ourDoctors/OurDoctors.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Container, Grid, Typography } from "@mui/material";
+import doctorImg from "../../../images/doctor-small.png";
+import OurDoctorSIngle from "./OurDoctorSIngle ";
 
+const doctorsData = [
+  {
+    name: "Dr. Coudi",
+    phone: "+880-188-934789",
+    image: doctorImg,
+  },
+  {
+    name: "Dr. Coudi",
+    phone: "+880-188-934789",
+    image: doctorImg,
+  },
+  {
+    name: "Dr. Coudi",
+    phone: "+880-188-934789",
+    image: doctorImg,
+  },
+];
 const OurDoctors = () => {
   return (
-    <div>
-      <h2>g</h2>
-    </div>
+    <Container>
+      <Typography
+        variant="h5"
+        color="#19D3AE"
+        sx={{ textAlign: "center", mt: 8, fontWeight: 500 }}
+      >
+        OUR DOCTORS
+      </Typography>
+      <Grid container spacing={3} sx={{ mt: 2 }}>
+        {doctorsData.map((item, i) => (
+          <OurDoctorSIngle key={i} item={item} />
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
