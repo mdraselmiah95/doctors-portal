903cb79f9c4f94cbded0a4d28010d8c65ce5a0e6
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 06:07:21 +0600

    WIP on Doctors portal project

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navigation from "../../Shared/Navigation/Navigation";
 import Appointment from "../appointment/Appointment";
-import AppointmentBanner from "../AppointmentBanner/AppointmentBanner";
 import Banner from "../Banner/Banner";
 import Doctors from "../doctors/Doctors";
 import InfoCard from "../InfoCard/InfoCard";
@@ -15,7 +14,6 @@ const Home = () => {
       <InfoCard />
       <Services />
       <Appointment />
-      <AppointmentBanner />
       <Doctors />
     </div>
   );
--- src/pages/Home/appointment/Appointment.js
+++ src/pages/Home/appointment/Appointment.js
@@ -1,6 +1,9 @@
-import { makeStyles } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import React from "react";
 import bg from "../../../images/appointment-bg.png";
+import doctorImg from "../../../images/doctor.png";
+import MuiButton from "../../../styledComponent/MuiButton";
 
 const Appointment = () => {
   const useStyle = makeStyles({
@@ -17,8 +20,33 @@ const Appointment = () => {
   });
   const { root } = useStyle();
   return (
-    <div>
-      <h2>gf</h2>
+    <div className={root}>
+      <Container>
+        <Grid container spacing={2} sx={{ alignItems: "center" }}>
+          <Grid item xs={12} sm={12} md={5} lg={5}>
+            <img
+              src={doctorImg}
+              alt=""
+              width="100%"
+              style={{ marginTop: -84 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={7} lg={7}>
+            <Typography variant="h6" color="#19D3AE">
+              Appointment
+            </Typography>
+            <Typography variant="h4" color="#fff">
+              Make an Appointment today
+            </Typography>
+            <Typography variant="body1" sx={{ my: 3, color: "#fff" }}>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+              laudantium consequatur? Explicabo neque quod ea.{" "}
+            </Typography>
+            <MuiButton variant="contained">Learn more</MuiButton>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
