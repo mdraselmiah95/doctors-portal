94d14ec2814443955fd38f2a7a070933574d9308
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 06:33:29 +0600

    WIP on Doctors portal project on Footer part

--- src/pages/Home/footer/Footer.js
+++ src/pages/Home/footer/Footer.js
@@ -1,6 +1,19 @@
+import { makeStyles } from "@mui/styles";
 import React from "react";
 
+const useStyle = makeStyles({
+  socialIcon: {
+    color: "#19D3AE !important",
+    border: "1px solid #19D3AE !important",
+    margin: "20px 10px 30px 0 !important",
+    "&:hover": {
+      background: "#19D3AE !important",
+      color: "#fff !important",
+    },
+  },
+});
 const Footer = () => {
+  const { socialIcon } = useStyle();
   return (
     <div>
       <h1>g</h1>
