97aaeee3d67a7dcff385b036a8572319dc1e8944
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 06:26:48 +0600

    WIP on Doctors portal project on Our Doctors and Our Single Doctor part

--- src/pages/Home/ourDoctors/OurDoctorSIngle .js
+++ src/pages/Home/ourDoctors/OurDoctorSIngle .js
@@ -1,10 +1,26 @@
 import React from "react";
 
-const OurDoctorSIngle = () => {
+const OurDoctorSIngle = ({ item }) => {
+  const { name, phone, image } = item;
   return (
-    <div>
-      <h1>f</h1>
-    </div>
+    <Grid item xs={12} sm={12} md={4} lg={4}>
+      <Paper elevation={0} sx={{ textAlign: "center" }}>
+        <img src={image} width="100%" alt="" />
+        <Typography variant="h5" fontWeight={600}>
+          {name}
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CallIcon sx={{ color: "#19D3AE" }} />
+          <Typography variant="body1">{phone}</Typography>
+        </div>
+      </Paper>
+    </Grid>
   );
 };
 
