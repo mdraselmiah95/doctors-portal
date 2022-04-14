7613866efd260f265d3b356b0bf1a3dbc090a50a
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 06:15:16 +0600

    WIP on Doctors portal project

---
+++ src/pages/Home/blogSingle/BlogSingle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogSingle = () => {
+  return (
+    <div>
+      <h2>gg</h2>
+    </div>
+  );
+};
+
+export default BlogSingle;
--- src/pages/Home/blogs/Blogs.js
+++ src/pages/Home/blogs/Blogs.js
@@ -1,7 +1,9 @@
+import { Container, Typography, Grid } from "@mui/material";
 import React from "react";
 import person1 from "../../../images/people-1.png";
 import person2 from "../../../images/people-2.png";
 import person3 from "../../../images/people-3.png";
+import BlogSingle from "../blogSingle/BlogSingle";
 
 const blogsData = [
   {
@@ -25,9 +27,29 @@ const blogsData = [
 ];
 const Blogs = () => {
   return (
-    <div>
-      <h1>g</h1>
-    </div>
+    <section id="blog">
+      <Container sx={{ py: 10, mt: 5 }}>
+        <Typography
+          variant="h5"
+          color="#19D3AE"
+          textAlign="center"
+          fontWeight={500}
+        >
+          OUR BLOG
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{ fontWeight: 500, textAlign: "center", mb: 2 }}
+        >
+          From Our Blog News
+        </Typography>
+        <Grid container spacing={3} sx={{ mt: 2, justifyContent: "center" }}>
+          {blogsData.map((item, i) => (
+            <BlogSingle key={i} item={item} />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 };
 
