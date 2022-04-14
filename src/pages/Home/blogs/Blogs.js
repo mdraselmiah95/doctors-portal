eb776cde5f912dfb2aa934e5a8dfaf2cb09b668c
mdraselmiah95 <russelroy15@gmail.com>
2022-04-14 06:13:25 +0600

    WIP on Doctors portal project

--- src/pages/Home/blogs/Blogs.js
+++ src/pages/Home/blogs/Blogs.js
@@ -1,5 +1,28 @@
 import React from "react";
+import person1 from "../../../images/people-1.png";
+import person2 from "../../../images/people-2.png";
+import person3 from "../../../images/people-3.png";
 
+const blogsData = [
+  {
+    name: "Dr. Sinthia",
+    date: "23 April 2019",
+    img: person3,
+    title: "Check at least a doctor in a year for your teeth",
+  },
+  {
+    name: "Dr. Monika",
+    date: "23 April 2019",
+    img: person2,
+    title: "Two time brush in a day can keep you healthy",
+  },
+  {
+    name: "Dr. John",
+    date: "23 April 2019",
+    img: person1,
+    title: "The tooth cancer is taking a challenge",
+  },
+];
 const Blogs = () => {
   return (
     <div>
