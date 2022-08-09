4efbe6acbfc5538916123df6387ed9f5c7ba556b
mdraselmiah95 <russelroy15@gmail.com>
2022-08-09 23:06:49 +0600

    new project added

--- src/pages/LogIn/Firebase/firebase.init.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import firebaseConfig from "./firebase.config";
-
-const initializeFirebase = () => {
-  initializeApp(firebaseConfig);
-};
-
-export default initializeFirebase;
