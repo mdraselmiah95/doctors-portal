1a9133839845d192304fa8ae5ec5e3b1e0a44b6a
mdraselmiah95 <russelroy15@gmail.com>
2022-08-10 16:45:02 +0600

    Doctors Protal latest project setup

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ function App() {
         >
           Learn React
         </a>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-green-500">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
