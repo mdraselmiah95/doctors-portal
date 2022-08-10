756d0aa687eeeff4fb51869ec9721749b1fb7f39
mdraselmiah95 <russelroy15@gmail.com>
2022-08-10 16:41:56 +0600

    Doctors Protal latest project setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
     </div>
   );
