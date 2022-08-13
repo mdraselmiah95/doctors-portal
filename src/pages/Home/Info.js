a64c87290ed236f3ceee26a752ad56d19865e0df
mdraselmiah95 <russelroy15@gmail.com>
2022-08-13 21:57:12 +0600

    WIP on Doctors Portal Services section

--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -1,13 +1,14 @@
 import React from "react";
 import InfoCard from "./InfoCard";
 
+//images and icons
 import clock from "../../assets/icons/clock.svg";
 import marker from "../../assets/icons/marker.svg";
 import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-5 px-12 lg:grid-cols-3">
       <InfoCard
         cardTitle="Opening Hours"
         bgClass="bg-gradient-to-r from-secondary to-primary"
