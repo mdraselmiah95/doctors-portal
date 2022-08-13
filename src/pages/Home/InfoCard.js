877fd6b400380fc13b02cf2ae63ec21cfa21a809
mdraselmiah95 <russelroy15@gmail.com>
2022-08-13 21:32:37 +0600

    WIP on Doctors Portal Info section

---
+++ src/pages/Home/InfoCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const InfoCard = ({ img, cardTitle, bgClass }) => {
+  return (
+    <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+      <figure className="pt-5 pl-5">
+        <img src={img} alt="Album" />
+      </figure>
+      <div className="text-white card-body">
+        <h2 className="card-title">{cardTitle}</h2>
+        <p>Click the button to listen on Spotiwhy app.</p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
