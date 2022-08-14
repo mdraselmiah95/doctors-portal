f92ae920437d0b4b4defa214d19ed3e6f6c76490
mdraselmiah95 <russelroy15@gmail.com>
2022-08-15 04:50:04 +0600

    WIP on Doctors Portal Appointment Services

--- src/pages/Appointment/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
+import Service from "./Service";
 
 const AvailableAppointments = ({ date }) => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div className="my-10">
       <h4 className="text-xl text-secondary text-center my-12">
         Available Appointments on: {format(date, "PP")}
       </h4>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
