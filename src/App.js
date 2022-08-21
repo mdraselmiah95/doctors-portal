4433d7a551cd270387e6256d689be5b2837797fc
mdraselmiah95 <russelroy15@gmail.com>
2022-08-22 00:42:28 +0600

    WIP on Doctors Portal Require Auth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./pages/About/About";
 import Appointment from "./pages/Appointment/Appointment";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import RequireAuth from "./pages/Login/RequireAuth";
 import SignUp from "./pages/Login/SignUp";
 import Navbar from "./pages/Shared/Navbar";
 
@@ -14,7 +15,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/appointment" element={<Appointment />} />
+        <Route
+          path="/appointment"
+          element={
+            <RequireAuth>
+              <Appointment />
+            </RequireAuth>
+          }
+        />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
