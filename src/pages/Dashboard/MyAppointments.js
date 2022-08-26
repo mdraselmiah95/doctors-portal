76edee50c95c9ffdf41ab3ef560814f3e01d8940
mdraselmiah95 <russelroy15@gmail.com>
2022-08-26 12:54:53 +0600

    WIP on Doctors Portal My Appointment

--- src/pages/Dashboard/MyAppointments.js
+++ src/pages/Dashboard/MyAppointments.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
   return (
     <div>
-      <h2>this is MyAppointments</h2>
+      <h2>this is MyAppointments{appointments.length}</h2>
     </div>
   );
 };
