ccf1d56bbf20ce1bd01addbd03ef36faea433bc9
mdraselmiah95 <russelroy15@gmail.com>
2022-08-28 13:53:55 +0600

    WIP on Doctors Portal Verity Token

--- src/pages/Dashboard/MyAppointments.js
+++ src/pages/Dashboard/MyAppointments.js
@@ -1,10 +1,13 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const MyAppointments = () => {
   const [appointments, setAppointments] = useState([]);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
@@ -14,10 +17,18 @@ const MyAppointments = () => {
           authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
       })
-        .then((res) => res.json())
+        .then((res) => {
+          console.log("response", res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          return res.json();
+        })
         .then((data) => setAppointments(data));
     }
-  }, [user]);
+  }, [navigate, user]);
   // console.log(appointments);
   const todayDate = new Date().toLocaleString("en-us", {
     month: "long",
