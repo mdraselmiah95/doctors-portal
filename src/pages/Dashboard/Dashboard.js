e8a7d504315ea28d6bd4a8f505d74b861ae1586c
mdraselmiah95 <russelroy15@gmail.com>
2022-08-28 16:20:14 +0600

    WIP on Doctors Portal Secure Admin

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  console.log(user);
+  const [admin] = useAdmin(user);
   return (
     <div className="drawer drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
       <div className="px-5 drawer-content lg:px-14 bg-dashboardBg">
-        <h2 className="py-5 pb-6 text-2xl font-bold text-purple-500 lg:pt-14">
-          Welcome to your Dashboard
+        <h2 className="py-5 pb-6 text-2xl font-bold lg:pt-14">
+          Welcome
+          <span className="font-mono underline"> {user.displayName} </span> to
+          your Dashboard.
         </h2>
         <Outlet />
       </div>
@@ -24,22 +32,19 @@ const Dashboard = () => {
           <li>
             <Link to="/dashboard/history">My History</Link>
           </li>
-          <li>
-            <Link to="/dashboard/users">All Users</Link>
-          </li>
-          {/* {admin && (
+          {admin && (
             <>
               <li>
                 <Link to="/dashboard/users">All Users</Link>
               </li>
-              <li>
+              {/* <li>
                 <Link to="/dashboard/addDoctor">Add a Doctor</Link>
               </li>
               <li>
                 <Link to="/dashboard/manageDoctor">Manage Doctors</Link>
-              </li>
+              </li> */}
             </>
-          )} */}
+          )}
         </ul>
       </div>
     </div>
