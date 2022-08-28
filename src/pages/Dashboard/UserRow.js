38abaa25d4ae77cfe697a1e23a873126663d894b
mdraselmiah95 <russelroy15@gmail.com>
2022-08-28 15:36:47 +0600

    WIP on Doctors Portal Make Admin

--- src/pages/Dashboard/UserRow.js
+++ src/pages/Dashboard/UserRow.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const UserRow = ({ user, refetch }) => {
   const { email, role } = user;
@@ -10,9 +11,18 @@ const UserRow = ({ user, refetch }) => {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to Make an admin");
+        }
+        return res.json();
+      })
+
       .then((data) => {
-        console.log(data);
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success(`Successfully made an admin`);
+        }
       });
   };
 
