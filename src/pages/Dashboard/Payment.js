7980967a736acabc8bc6d04dfdc2a44082c28c16
mdraselmiah95 <russelroy15@gmail.com>
2022-09-01 19:11:26 +0600

    WIP on Doctors Portal Payment

--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -21,7 +21,27 @@ const Payment = () => {
   }
   return (
     <div>
-      <h2>Payment {id}</h2>
+      <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+        <div className="card-body">
+          <p className="text-success font-bold">
+            Hello, {appointment.patientName}
+          </p>
+          <h2 className="card-title">Please Pay for {appointment.treatment}</h2>
+          <p>
+            Your Appointment:
+            <span className="text-orange-700">{appointment.date}</span> at
+            {appointment.slot}
+          </p>
+          <p>Please pay: ${appointment.price}</p>
+        </div>
+      </div>
+      <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div className="card-body">
+          {/* <Elements stripe={stripePromise}>
+            <CheckoutForm appointment={appointment} />
+          </Elements> */}
+        </div>
+      </div>
     </div>
   );
 };
