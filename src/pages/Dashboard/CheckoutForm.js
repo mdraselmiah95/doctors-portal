fc4c33e5a82497833f553d86026473b223c93d77
mdraselmiah95 <russelroy15@gmail.com>
2022-09-01 19:58:15 +0600

    WIP on Doctors Portal Payment using Stripe

---
+++ src/pages/Dashboard/CheckoutForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-success btn-sm mt-4"
+          type="submit"
+          //   disabled={!stripe || !clientSecret || success}
+        >
+          Pay
+        </button>
+      </form>
+      {/* {cardError && <p className="text-red-500">{cardError}</p>} */}
+      {/* {success && (
+        <div className="text-green-500">
+          <p>{success} </p>
+          <p>
+            Your transaction Id:{" "}
+            <span className="text-orange-500 font-bold">{transactionId}</span>
+          </p>
+        </div>
+      )} */}
+    </>
+  );
+};
+
+export default CheckoutForm;
+<h2>Checkout</h2>;
--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../Shared/Loading";
+import CheckoutForm from "./CheckoutForm";
+
+// Connect with Stripe
+const stripePromise = loadStripe(
+  "pk_test_51Jw6SKHMt687dAra9sxMSE3nLy5pM9HeNqThwsGNox6xrDosPrH58ciSutmFgE9p4qhGYEBunlsp1WXeNIHBZSdG00BnwCZuqs"
+);
 
 const Payment = () => {
   const { id } = useParams();
@@ -22,7 +30,7 @@ const Payment = () => {
   }
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="">
       <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
         <div className="card-body">
           <p className="text-success font-bold">
@@ -39,9 +47,9 @@ const Payment = () => {
       </div>
       <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
         <div className="card-body">
-          {/* <Elements stripe={stripePromise}>
+          <Elements stripe={stripePromise}>
             <CheckoutForm appointment={appointment} />
-          </Elements> */}
+          </Elements>
         </div>
       </div>
     </div>
