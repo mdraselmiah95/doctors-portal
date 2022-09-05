49d387da4820d8727fa53496651ce3c0fcdb2107
mdraselmiah95 <russelroy15@gmail.com>
2022-09-05 15:31:36 +0600

    new code added

--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -1,7 +1,7 @@
 import React from "react";
 import InfoCard from "./InfoCard";
 
-//images and icons
+// Images and icons
 import clock from "../../assets/icons/clock.svg";
 import marker from "../../assets/icons/marker.svg";
 import phone from "../../assets/icons/phone.svg";
